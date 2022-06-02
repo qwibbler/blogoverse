79ccad84fd381751d7bec81aa0726dc68fcc6f73
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-02 14:51:19 -0600

    Add tables for db as per diagram

---
+++ db/migrate/20220602195719_create_post.rb
@@ -0,0 +1,13 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.references :user, foreign_key: true, index: true
+      t.string :title
+      t.string :text
+      t.timestamps
+      t.integer :comments_counter
+      t.integer :likes_counter
+    end
+    # add_reference :posts, :user, null: false, foreign_key: true
+  end
+end
