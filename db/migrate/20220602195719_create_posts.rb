a352eefe26875892192fcd734a572370c60ba911
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-06 08:38:56 -0600

    Rename migration files

--- db/migrate/20220602195719_create_posts.rb
+++ db/migrate/20220602195719_create_posts.rb
@@ -1,4 +1,4 @@
-class CreatePost < ActiveRecord::Migration[7.0]
+class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.references :user, foreign_key: true, index: true
