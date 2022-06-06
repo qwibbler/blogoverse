634d7f3860e22c4524aed9aef7dc2d4e79db9a18
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-06 11:29:08 -0600

    Get Comment method working

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_comments_count(update_post)
-    new = comment.where(post_id: update_post.id).count
-    post.update(comments_counter: new)
+  def update_comments_count
+    new = Comment.where(post_id: self.post_id).count
+    Post.update(comments_counter: new)
   end
 end
