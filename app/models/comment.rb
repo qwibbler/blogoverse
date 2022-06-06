9b3b402cf8a2a0142a08821d3677db65160bd401
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-06 09:07:47 -0600

    Add comments methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_comments_count(update_post)
+    new = comment.where(post_id: update_post.id).count
+    post.update(comments_counter: new)
+  end
 end
