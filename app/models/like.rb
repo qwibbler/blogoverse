13b99ea399b1575a9e87d13c2d785c76fa5ad218
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-06 11:31:38 -0600

    Get Like Method working

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_likes_count(update_post)
-    new = like.where(post_id: update_post.id).count
-    post.update(likes_counter: new)
+  def update_likes_count
+    new = Like.where(post_id: self.post_id).count
+    Post.find(self.post_id).update(likes_counter: new)
   end
 end
