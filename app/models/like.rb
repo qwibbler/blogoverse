776d5af2312e057b33787a42fe6cc4f98be966f1
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-06 09:08:50 -0600

    Add likes methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_likes_count(update_post)
+    new = like.where(post_id: update_post.id).count
+    post.update(likes_counter: new)
+  end
 end
