49b94fb122dda40e51e0755c9388fd8173be4f63
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-07 12:19:56 -0600

    Make working basic posts index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
-  def show; end
-  def index; end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user_id: params[:user_id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.recent_posts
+  end
 
   def index
     @users = User.all
