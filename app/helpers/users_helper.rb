59848df295618d6d7c5cab8e00e24ddbdcbbb32c
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-07 17:05:54 -0600

    Final Styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,10 @@
 module UsersHelper
   def see_all_btn
     out = ''
-    out << link_to('See All Posts', user_posts_path(@user), class: 'btn') if @posts.length > 0
+    unless @user.posts_counter.nil? || @user.posts_counter <= 3
+      out << link_to('See All Posts', user_posts_path(@user),
+                     class: 'btn')
+    end
     out.html_safe
   end
 end
