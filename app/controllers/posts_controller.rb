64db24faa7e0caa90046fb94f58154fe799f835d
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-08 18:58:26 -0600

    Create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @new_post = @user.posts.new(params.require(:post).permit(:title, :text))
+    respond_to do |format|
+      format.html do
+        if @new_post.save
+          flash[:success] = t('post_success')
+          redirect_to [@user, @new_post]
+        else
+          flash.now[:error] = t('post_error')
+          render :new
+        end
+      end
+    end
+    @new_post
+  end
+
   def show
     @post = Post.find(params[:id])
     @post.update_likes_count
