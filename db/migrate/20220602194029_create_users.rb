65dbc73f46b00950a608cd0ffb3dbfd0f3bce2c3
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-09 11:27:58 -0600

    Add defaults

--- db/migrate/20220602194029_create_users.rb
+++ db/migrate/20220602194029_create_users.rb
@@ -2,10 +2,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.string :bio
+      t.string :photo, :default => ''
+      t.string :bio, :default => ''
+      t.integer :posts_counter, :default => 0
       t.timestamps
-      t.integer :posts_counter
     end
   end
 end
