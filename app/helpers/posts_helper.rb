ff39b27e02c4de124ed4e43d9b03bba54a65f040
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-14 11:05:53 -0600

    Style more

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,11 +6,13 @@ module PostsHelper
   end
 
   def like_btn
-    if @post.liked?(current_user)
-      button_to(t('liked'), user_post_likes_path(@post.user, @post), method: :post, disabled: true,
-                                                                     class: 'btn sketchy')
-    else
-      button_to(t('like'), user_post_likes_path(@post.user, @post), method: :post, class: 'btn sketchy')
+    if current_user
+      if @post.liked?(current_user)
+        button_to(t('liked'), user_post_likes_path(@post.user, @post), method: :post, disabled: true,
+                                                                      class: 'btn sketchy')
+      else
+        button_to(t('like'), user_post_likes_path(@post.user, @post), method: :post, class: 'btn sketchy')
+      end
     end
   end
 end
