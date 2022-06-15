b6b2c8c5a1d9a2ac236563dab0d3d51001a55560
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-15 13:56:00 -0600

    Add first login inputs tests

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'Hello world', type: :system do
-  describe 'index page' do
-    it 'shows the right content' do
-      visit users_path
-      sleep(5)
-      expect(page).to have_content('blogverse')
+RSpec.describe 'Login Page', type: :system do
+  describe 'has  email and password inputs and the "Submit" button.' do
+    before_action { visit new_user_session_path }
+    it 'has label for email' do
+      expect(page).to have_content('Email')
+    end
+    it 'has label for password' do
+      expect(page).to have_content('Password')
+    end
+    it 'has input for email' do
+      expect(page).to have_selector('input[type="email"]')
+    end
+    it 'has input for password' do
+      expect(page).to have_selector('input[type="password"]')
+    end
+    it 'has button for login' do
+      expect(page).to have_selector('input[type="submit"]')
     end
   end
 end
