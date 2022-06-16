4577560f627d61ace8ecf7504905c83e899b9be3
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-16 16:56:42 -0600

    Add JWT token to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true }
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
+  after_save :add_token
+
   ROLES = %i[admin user].freeze
 
   def is?(requested_role)
@@ -26,5 +28,10 @@ class User < ApplicationRecord
 
   def recent_posts
     posts.limit(3).order(created_at: :desc)
+  end
+
+  def add_token
+    token = JWT.encode email, nil, 'none'
+    update_column(:token, token)
   end
 end
