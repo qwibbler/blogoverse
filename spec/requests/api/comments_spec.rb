6cd5ad930d049cf65581661e07ef74ef44cfae66
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-17 16:07:30 -0600

    Try to fix linters and tests

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -1,61 +1,84 @@
 require 'swagger_helper'
 
-comment_info = {
-  id: { type: :integer },
-  user_id: { type: :integer },
-  post_id: { type: :integer },
-  text: { type: :string },
-  created_at: { type: :datetime },
-  updated_at: { type: :datetime }
-}
-
-jason = User.create(name: 'Jason', email: 'jason@example.com', password: 'password', confirmed_at: Time.now)
-post = Post.create(user: jason, title: 'title-first', text: 'test').id
-
 RSpec.describe 'API Comments', type: :request do
   path '/api/posts/{id}/comments' do
     get 'Retrieves all comments of a post' do
-      tags 'Show all comments'
+      tags 'Comments'
       consumes 'multipart/form-data'
       parameter name: :id, in: :path, type: :string, required: true
       parameter name: :token, in: :formData, type: :string, required: true
 
       response '200', 'OK' do
-        schema type: :array, items: comment_info, required: %w[id user_id post_id text created_at updated_at]
-        let(:token) { jason.token }
-        let(:id) { post.id }
+        schema type: :array,
+               items: {
+                 id: { type: :integer },
+                 user_id: { type: :integer },
+                 post_id: { type: :integer },
+                 text: { type: :string },
+                 created_at: { type: :datetime },
+                 updated_at: { type: :datetime }
+               },
+               required: %w[id user_id post_id text created_at updated_at]
+        let(:token) do
+          User.create(name: 'Jason', email: 'jason@example.com', password: 'password', confirmed_at: Time.now).token
+        end
+
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user:, title: 'title-first', text: 'test').id
+        end
         run_test!
       end
 
       response '400', 'Invalid Token' do
         let(:token) { '0' }
-        let(:id) { post.id }
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user:, title: 'title-first', text: 'test').id
+        end
         run_test!
       end
     end
 
     post 'Posts a new comment' do
-      tags 'Post new comment'
+      tags 'Comments'
       consumes 'multipart/form-data'
       parameter name: :id, in: :path, type: :string, required: true
       parameter name: :token, in: :formData, type: :string, required: true
       parameter name: :text, in: :formData, type: :string, required: true
 
       response '201', 'Created' do
-        schema type: :object, properties: {
-          success: { type: :boolean },
-          data: {
-            type: :object, properties: {
-              comment: {
-                type: :object, properties: comment_info,
-                required: %w[id user_id post_id text created_at updated_at]
-              }
-            }
-          }
-        },
+        schema type: :object,
+               properties: {
+                 success: { type: :boolean },
+                 data: {
+                   type: :object,
+                   properties: {
+                     comment: {
+                       type: :object,
+                       properties: {
+                         id: { type: :integer },
+                         user_id: { type: :integer },
+                         post_id: { type: :integer },
+                         text: { type: :string },
+                         created_at: { type: :datetime },
+                         updated_at: { type: :datetime }
+                       },
+                       required: %w[id user_id post_id text created_at updated_at]
+                     }
+                   }
+                 }
+               },
                required: []
-        let(:token) { jason.token }
-        let(:id) { post.id }
+        let(:token) do
+          User.create(name: 'Jason', email: 'jason@example.com', password: 'password', confirmed_at: Time.now).token
+        end
+
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user:, title: 'title-first', text: 'test').id
+        end
+
         let(:text) { 'This is the comment.' }
         run_test!
       end
@@ -63,14 +86,22 @@ RSpec.describe 'API Comments', type: :request do
       response '400', 'Invalid Token' do
         let(:token) { '0' }
         let(:text) { 'This is the comment' }
-        let(:token) { jason.token }
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user:, title: 'title-first', text: 'test').id
+        end
         run_test!
       end
 
       response '400', 'Comment must have text' do
-        let(:token) { jason.token }
-        let(:id) { post.id }
-        let(:text) { nil }
+        let(:token) do
+          User.create(name: 'Jason', email: 'jason@example.com', password: 'password', confirmed_at: Time.now).token
+        end
+        let(:text) {}
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user:, title: 'title-first', text: 'test').id
+        end
         run_test!
       end
     end
