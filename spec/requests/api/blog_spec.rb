ca866337eae4a0c8a40a0e0a9232704f367043f5
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-17 14:59:11 -0600

    Add  api comments index tests

--- spec/requests/api/blog_spec.rb
+++ spec/requests/api/blog_spec.rb
@@ -31,4 +31,44 @@ RSpec.describe 'Blog API', type: :request do
       end
     end
   end
+
+  path '/api/posts/{id}/comments' do
+    get 'Retrieves all posts' do
+      tags 'Comments'
+      consumes 'multipart/form-data'
+      parameter name: :id, in: :path, type: :string, required: true
+      parameter name: :token, in: :formData, type: :string, required: true
+
+      response '200', 'OK' do
+        schema type: :array,
+               items: {
+                 id: { type: :integer },
+                 user_id: { type: :integer },
+                 post_id: { type: :integer },
+                 text: { type: :string },
+                 created_at: { type: :datetime },
+                 updated_at: { type: :datetime }
+               },
+               required: %w[id user_id post_id text created_at updated_at]
+        let(:token) do
+          User.create(name: 'Empty', email: 'empty@example.com', password: 'password', confirmed_at: Time.now).token
+        end
+
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user: user, title: 'title-first', text: 'test').id
+        end
+        run_test!
+      end
+
+      response '400', 'Invalid Token' do
+        let(:token) { '0' }
+        let(:id) do
+          user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password', confirmed_at: Time.now)
+          Post.create(user: user, title: 'title-first', text: 'test').id
+        end
+        run_test!
+      end
+    end
+  end
 end
