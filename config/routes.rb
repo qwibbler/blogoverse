eebc3502fa4f64891f439ad2a51ac0f4729e618a
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-17 13:21:38 -0600

    Update readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, param: :_username, only: [:index, :show] do
+  resources :users, only: [:index, :show] do
     resources :posts do
       resources :comments, only: [:create, :destroy, :edit]
       resources :likes, only: [:create, :destroy]
